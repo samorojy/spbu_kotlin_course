873706e5f92777735d21d5b525aa935973c374dc
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2021-03-02 20:37:18 +0300

    added const to fix magicNumber

--- src/main/kotlin/firsthomework/task_3.kt
+++ src/main/kotlin/firsthomework/task_3.kt
@@ -5,26 +5,33 @@ import action.InsertAtStart
 import action.InsertAtEnd
 import action.Move
 
+const val NUMBER0 = 0
+const val NUMBER1 = 1
+const val NUMBER2 = 2
+const val NUMBER3 = 3
+const val NUMBER4 = 4
+const val NUMBER5 = 5
+
 fun main() {
     val storage = CommandStorage()
     storage.actionList
-    InsertAtStart(5, storage).doAction()
-    InsertAtStart(3, storage).doAction()
-    InsertAtStart(6, storage).doAction()
-    InsertAtStart(7, storage).doAction()
+    InsertAtStart(NUMBER1, storage).doAction()
+    InsertAtStart(NUMBER2, storage).doAction()
+    InsertAtStart(NUMBER3, storage).doAction()
+    InsertAtStart(NUMBER4, storage).doAction()
     storage.numberList.forEach { println(it) }
     storage.undoLastAction()
     println("After Undo")
     storage.numberList.forEach { println(it) }
-    InsertAtEnd(10, storage).doAction()
+    InsertAtEnd(NUMBER5, storage).doAction()
     println("After Insert At end")
     storage.numberList.forEach { println(it) }
-    Move(0, 3, storage).doAction()
+    Move(NUMBER0, NUMBER3, storage).doAction()
     storage.numberList.forEach { println(it) }
     println("After move")
     storage.undoLastAction()
     storage.numberList.forEach { println(it) }
-    InsertAtEnd(0, storage).doAction()
+    InsertAtEnd(NUMBER0, storage).doAction()
     storage.numberList.forEach { println(it) }
     storage.undoLastAction()
     storage.numberList.forEach { println(it) }
