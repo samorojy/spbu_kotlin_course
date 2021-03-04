f71b544920a0734d92e46578e047468830f8d9f3
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2021-03-04 15:48:00 +0300

    fix style

--- src/main/kotlin/action/actions.kt
+++ src/main/kotlin/action/actions.kt
@@ -35,9 +35,7 @@ private fun moveElement(startIndex: Int, endIndex: Int, storage: CommandStorage)
 class Move(private val startIndex: Int, private val endIndex: Int, override val storage: CommandStorage) : Action {
 
     override fun doAction() {
-        if (startIndex < 0 || endIndex < 0 || startIndex > storage.numberList.size ||
-            endIndex > storage.numberList.size
-        ) {
+        if ((startIndex !in (0..storage.numberList.size)) || (endIndex !in 0..storage.numberList.size)) {
             error("ERROR! Out of List bounds")
         }
         moveElement(startIndex, endIndex, storage)
