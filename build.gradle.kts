e28bcb9fd1af914b39c5454405cd405ca57f062f
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2021-03-19 18:00:42 +0300

    Ilya poprosil 0_0

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,15 @@ detekt {
 
 tasks.test {
     useJUnitPlatform()
+    testLogging {
+        events(
+            org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_ERROR,
+            org.gradle.api.tasks.testing.logging.TestLogEvent.STARTED,
+            org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED,
+            org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED,
+            org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED
+        )
+    }
 }
 
 tasks.withType<KotlinCompile>() {
