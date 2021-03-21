3e2d7a84d3595c3ba7f3f2ad1ab8373ca8a580f9
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2021-03-21 04:00:40 +0300

    demo_task.kt fixes

--- src/main/kotlin/homework3/demo_task.kt
+++ src/main/kotlin/homework3/demo_task.kt
@@ -2,6 +2,6 @@ package homework3
 
 fun main() {
     val inputFileName = "src/main/resources/homework3/Config.Yaml"
-    val outputFileName = "src/main/resources"
+    val outputFileName = "src/main/resources/"
     generateTest(inputFileName, outputFileName)
 }
