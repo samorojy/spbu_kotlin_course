2dad00f07201ad8ababeda8e31385e9368c4e70a
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2021-03-23 16:23:37 +0300

    added temporary directory

--- src/test/kotlin/action/CommandStorageTest.kt
+++ src/test/kotlin/action/CommandStorageTest.kt
@@ -2,20 +2,22 @@ package action
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.io.TempDir
 import java.io.File
+import java.nio.file.Path
 
 internal class CommandStorageTest {
 
     @Test
-    fun serializeToFile() {
+    fun serializeToFile(@TempDir directory: Path) {
         val testStorage = CommandStorage()
+        val file: Path = directory.resolve("testFile.json")
         InsertAtStart(1).doAction(testStorage)
         InsertAtStart(2).doAction(testStorage)
         InsertAtStart(3).doAction(testStorage)
         InsertAtEnd(5).doAction(testStorage)
-        testStorage.serializeToFile("test.json")
-        assertEquals(javaClass.getResource("ActionList.json").readText(), File("test.json").readText())
-        File("test.json").delete()
+        testStorage.serializeToFile(file.toString())
+        assertEquals(javaClass.getResource("ActionList.json").readText(), File(file.toString()).readText())
     }
 
     @Test
