d1de74aac1a4aeecd56d682461c8eb8dfe95331e
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2021-03-23 23:00:15 +0300

    removed duplicate code in tests

--- src/test/kotlin/homework3/TestGeneratorTest.kt
+++ src/test/kotlin/homework3/TestGeneratorTest.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
+import java.io.File
 
 internal class TestGeneratorTest {
 
@@ -11,7 +12,7 @@ internal class TestGeneratorTest {
         @JvmStatic
         fun inputData(): List<Arguments> = listOf(
             Arguments.of(
-                TestGeneratorTest::class.java.getResource("GeneratorTest1.kt").readText().replace("\r\n", "\n"),
+                TestGeneratorTest::class.java.getResource("GeneratorTest1.kt").file,
                 Config(
                     "homework3",
                     "PerformedCommandStorage",
@@ -22,7 +23,7 @@ internal class TestGeneratorTest {
                 )
             ),
             Arguments.of(
-                TestGeneratorTest::class.java.getResource("GeneratorTest2.kt").readText().replace("\r\n", "\n"),
+                TestGeneratorTest::class.java.getResource("GeneratorTest2.kt").file,
                 Config(
                     "homework3",
                     "CommandStorage",
@@ -36,7 +37,7 @@ internal class TestGeneratorTest {
 
     @MethodSource("inputData")
     @ParameterizedTest(name = "test{index}, {1}")
-    fun getKotlinFile(expected: String, input: Config) {
-        assertEquals(expected, TestGenerator(input).kotlinFile.toString())
+    fun getKotlinFile(expected: File, input: Config) {
+        assertEquals(expected.readText().replace("\r\n", "\n"), TestGenerator(input).kotlinFile.toString())
     }
 }
