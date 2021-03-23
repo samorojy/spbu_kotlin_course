689c8c92fb0808e6698a2dc85cc24058aebf7a5c
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2021-03-23 22:49:37 +0300

    Added the ability for the user to write to the console the path to the input file and output file

--- src/main/kotlin/homework3/demoTask.kt
+++ src/main/kotlin/homework3/demoTask.kt
@@ -1,7 +1,10 @@
 package homework3
 
 fun main() {
-    val inputFileName = "src/main/resources/homework3/Config.Yaml"
-    val outputFileName = "src/main/resources/"
+    val scanner = java.util.Scanner(System.`in`)
+    println("Please enter path to input file")
+    val inputFileName = scanner.nextLine()
+    println("Please enter path to output file")
+    val outputFileName = scanner.nextLine()
     generateTest(inputFileName, outputFileName)
 }
