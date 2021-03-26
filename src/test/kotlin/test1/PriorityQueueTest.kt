68477301c0e38ce829fa38865fbb71796c73e652
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2021-03-26 13:07:28 +0300

    add test

---
+++ src/test/kotlin/test1/PriorityQueueTest.kt
@@ -0,0 +1,52 @@
+package test1
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.assertThrows
+
+internal class PriorityQueueTest {
+    private val queue = PriorityQueue<String, Int>()
+
+    @Test
+    fun enqueue() {
+        queue.enqueue("F#", 1)
+        assertEquals("F#", queue.peek())
+    }
+
+    @Test
+    fun peek() {
+        queue.enqueue("Kotlin", 10)
+        queue.enqueue("F#", 1)
+        assertEquals("Kotlin", queue.peek())
+    }
+
+    @Test
+    fun peekEmpty() {
+        assertThrows<NoSuchElementException> { queue.peek() }
+    }
+
+    @Test
+    fun remove() {
+        queue.enqueue("Kotlin", 10)
+        queue.enqueue("F#", 1)
+        queue.remove()
+        assertEquals("F#", queue.peek())
+    }
+
+    @Test
+    fun removeEmpty() {
+        assertThrows<NoSuchElementException> { queue.remove() }
+    }
+
+    @Test
+    fun rool() {
+        queue.enqueue("Kotlin", 10)
+        queue.enqueue("F#", 1)
+        assertEquals("Kotlin", queue.rool())
+    }
+
+    @Test
+    fun roolEmpty() {
+        assertThrows<NoSuchElementException> { queue.rool() }
+    }
+}
