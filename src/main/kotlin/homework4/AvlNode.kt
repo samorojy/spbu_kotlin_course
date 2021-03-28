6146e0a3f42aaac47cfad7d2b25e2a3d4c54c89e
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2021-03-29 02:46:55 +0300

    add half part of work

--- src/main/kotlin/homework4/AvlNode.kt
+++ src/main/kotlin/homework4/AvlNode.kt
@@ -1,99 +1,163 @@
 package homework4
 
-import kotlin.math.min
+class AvlNode<K : Comparable<K>, V>(private var key: K, var value: V) {
+    private var height: Int = 0
+    private var leftNode: AvlNode<K, V>? = null
+    private var rightNode: AvlNode<K, V>? = null
 
-class AvlNode<K : Comparable<K>, V>(val key: K, var value: V) {
-    var height: Int = 0
-    var leftNode: AvlNode<K, V>? = null
-    var rightNode: AvlNode<K, V>? = null
-
-    fun add(key: K, newValue: V): Boolean? {
-        return when {
-            key == this.key -> {
-                value = newValue
-                true
-            }
-
-            key < this.key -> {
-                if (leftNode == null) {
-                    leftNode = AvlNode(key, newValue)
-                    true
+    fun balance(): AvlNode<K, V> {
+        return when (this.getBalanceFactor()) {
+            2 -> {
+                if (this.leftNode?.getBalanceFactor() == -1) {
+                    this.leftRightRotate()
                 } else {
-                    leftNode?.add(key, newValue)
+                    this.rightRotate()
                 }
             }
-
-            key > this.key -> {
-                if (rightNode == null) {
-                    rightNode = AvlNode(key, newValue)
-                    true
+            -2 -> {
+                if (this.rightNode?.getBalanceFactor() == 1) {
+                    this.rightLeftRotate()
                 } else {
-                    rightNode?.add(key, newValue)
+                    this.leftRotate()
                 }
             }
-            else -> false
+            else -> this
         }
     }
 
-    private fun getParentOfKey(key: K): AvlNode<K, V>? {
-        if (key != this.key) {
-            if (key == this.rightNode?.key || key == this.leftNode?.key) {
-                return this
-            }
-            if (key < this.key) {
-                if (this.leftNode == null) {
-                    return null
-                }
-                return this.leftNode?.getParentOfKey(key)
-            }
-            if (key > this.key) {
-                if (this.rightNode == null) {
-                    return null
-                }
-                return this.rightNode?.getParentOfKey(key)
-            }
-        }
-        return this
+    private fun leftRotate(): AvlNode<K, V> {
+        val pivot = this.rightNode!!
+        this.rightNode = pivot.leftNode
+        pivot.leftNode = this
+        this.height = maxOf(this.leftNode?.height ?: 0, this.rightNode?.height ?: 0) + 1
+        pivot.height = maxOf(pivot.leftNode?.height ?: 0, pivot.rightNode?.height ?: 0) + 1
+        return pivot
     }
 
-    fun removeNode(key: K): Boolean {
-        val removeNodeParent = getParentOfKey(key) ?: return false
-        if (key == removeNodeParent.key) {
-            return remove(null,removeNodeParent)
-        }
-        if (key == removeNodeParent.leftNode?.key) {
-            return remove(removeNodeParent,removeNodeParent.leftNode!!)
-        }
-        if (key == removeNodeParent.rightNode?.key) {
-            return remove(removeNodeParent, removeNodeParent.rightNode!!)
-        }
-        return false
+    private fun leftRightRotate(): AvlNode<K, V> {
+        val leftChild = this.leftNode ?: return this
+        this.leftNode = leftChild.rightRotate()
+        return this.rightRotate()
     }
 
-    private fun remove(parentNode:AvlNode<K,V>?,node: AvlNode<K, V>): Boolean {
-
-        if (this.leftNode != null && this.rightNode != null) {
-            val minimumNodeParent = this.rightNode?.getMinimumParent()
-            val newNode = minimumNodeParent?.leftNode
-            if (newNode?.rightNode != null) {
-                minimumNodeParent.leftNode = newNode.rightNode
-            } else {
-                minimumNodeParent?.leftNode = null
-            }
-            this.leftNode = newNode?.leftNode
-            this.rightNode = newNode?.rightNode
-            return true
-        }
-        return false
+    private fun rightRotate(): AvlNode<K, V> {
+        val pivot = this.leftNode!!
+        this.leftNode = pivot.rightNode
+        pivot.rightNode = this
+        this.height = maxOf(this.leftNode?.height ?: 0, this.rightNode?.height ?: 0) + 1
+        pivot.height = maxOf(pivot.leftNode?.height ?: 0, pivot.rightNode?.height ?: 0) + 1
+        return pivot
     }
 
-    private fun getMinimumParent(): AvlNode<K, V>? {
+    private fun rightLeftRotate(): AvlNode<K, V> {
+        val rightChild = this.rightNode ?: return this
+        this.rightNode = rightChild.rightRotate()
+        return this.leftRotate()
+    }
+
+    fun updateHeight() {
         if (this.leftNode != null) {
-            if (this.leftNode?.leftNode == null) {
-                return this
+            this.leftNode!!.updateHeight()
+        }
+        if (this.rightNode != null) {
+            this.rightNode!!.updateHeight()
+        }
+        height = maxOf(leftNode?.height ?: 0, rightNode?.height ?: 0) + 1
+    }
+
+    private fun getBalanceFactor(): Int {
+        return (leftNode?.height ?: 0) - (rightNode?.height ?: 0)
+    }
+
+    fun add(key: K, newValue: V): Boolean {
+        if (key == this.key) {
+            value = newValue
+            return false
+        }
+        if (key < this.key) {
+            if (leftNode == null) {
+                leftNode = AvlNode(key, newValue)
+                return true
+            } else {
+                leftNode?.add(key, newValue)
             }
-            return leftNode?.getMinimumParent()
+        }
+
+        if (key > this.key) {
+            if (rightNode == null) {
+                rightNode = AvlNode(key, newValue)
+                return true
+            } else {
+                rightNode?.add(key, newValue)
+            }
+        }
+        return false
+    }
+
+    fun remove(key: K): AvlNode<K, V>? {
+        if (key < this.key) {
+            leftNode = leftNode?.remove(key)
+            return this
+        }
+        if (key > this.key) {
+            rightNode = rightNode?.remove(key)
+            return this
+        }
+        if (leftNode == null) {
+            return rightNode
+        }
+        if (rightNode == null) {
+            return leftNode
+        }
+        val minimumNode = rightNode!!.getMinimumNode()
+        this.value = minimumNode.value
+        this.key = minimumNode.key
+        if (rightNode?.key == minimumNode.key) {
+            this.rightNode = rightNode?.rightNode
+        } else {
+            rightNode?.removeMinimumNode(this.key)
         }
         return this
+    }
+
+    private fun getMinimumNode(): AvlNode<K, V> {
+        while (this.leftNode != null) {
+            this.leftNode!!.getMinimumNode()
+        }
+        return this
+    }
+
+    private fun removeMinimumNode(minKey: K) {
+        if (leftNode?.key == minKey) {
+            leftNode = null
+        } else {
+            leftNode?.removeMinimumNode(minKey)
+        }
+    }
+
+    fun isContainsKey(key: K): Boolean {
+        if (key < this.key) {
+            return this.leftNode?.isContainsKey(key) ?: false
+        }
+        if (key > this.key) {
+            return this.rightNode?.isContainsKey(key) ?: false
+        }
+        return true
+    }
+
+    fun isContainsValue(value: V): Boolean {
+        return this.value == value ||
+                rightNode?.isContainsValue(value) ?: false ||
+                leftNode?.isContainsValue(value) ?: false
+    }
+
+    fun getValueByKey(key: K): V? {
+        if (key < this.key) {
+            return this.leftNode?.getValueByKey(key)
+        }
+        if (key > this.key) {
+            return this.rightNode?.getValueByKey(key)
+        }
+        return this.value
     }
 }
--- src/main/kotlin/homework4/AvlTree.kt
+++ src/main/kotlin/homework4/AvlTree.kt
@@ -2,24 +2,40 @@ package homework4
 
 class AvlTree<K : Comparable<K>, V> : Map<K, V> {
     private var root: AvlNode<K, V>? = null
-    override var size: Int = 0
-    override val entries: Set<Map.Entry<K, V>> = mutableSetOf()
-    override val keys: Set<K> = mutableSetOf()
-    override val values: Set<V> = mutableSetOf()
+    private var _size: Int = 0
+    override val size: Int
+        get() {
+            return _size
+        }
+    override var entries: Set<Map.Entry<K, V>> = setOf()
+    override val keys: MutableSet<K> = mutableSetOf()
+    override val values: Collection<V> = mutableListOf()
 
-    override fun containsKey(key: K): Boolean {
-        TODO("Not yet implemented")
+    fun addValue(key: K, value: V) {
+        if (this.isEmpty()) {
+            root = AvlNode(key, value)
+            this._size++
+        } else {
+            if (root?.add(key, value) == true) {
+                _size++
+            }
+            root?.updateHeight()
+            root = root?.balance()
+        }
     }
 
-    override fun containsValue(value: V): Boolean {
-        TODO("Not yet implemented")
+    fun remove(key: K) {
+        root = root?.remove(key)
+        root?.updateHeight()
+        root = root?.balance()
     }
+
+    override fun containsKey(key: K): Boolean = root?.isContainsKey(key) ?: false
+
+    override fun containsValue(value: V): Boolean = root?.isContainsValue(value) ?: false
 
     override fun isEmpty(): Boolean = root == null
 
-    override fun get(key: K): V? {
-        TODO("Not yet implemented")
-    }
-
+    override fun get(key: K): V? = root?.getValueByKey(key)
 
 }
