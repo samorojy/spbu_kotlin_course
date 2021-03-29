3a36759df2658530d2da644d4dcb892e35364edc
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2021-03-29 22:08:25 +0300

    new GENERAL test

--- src/test/kotlin/homework3/TestGeneratorTest.kt
+++ src/test/kotlin/homework3/TestGeneratorTest.kt
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
-import java.io.File
 
 internal class TestGeneratorTest {
 
---
+++ src/test/resources/homework3/generalTest/test2/expected.kt
@@ -0,0 +1,17 @@
+package test2
+
+import org.junit.jupiter.api.Test
+
+internal class AvlTreeTest {
+  @Test
+  fun remove() {
+  }
+
+  @Test
+  fun add() {
+  }
+
+  @Test
+  fun balance() {
+  }
+}
