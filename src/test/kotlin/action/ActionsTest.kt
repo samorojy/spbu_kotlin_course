841f08c676084f224c32499d62a4f7ee5e6304fb
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2021-04-02 22:21:23 +0300

    All tests except serialization works

--- src/test/kotlin/action/ActionsTest.kt
+++ src/test/kotlin/action/ActionsTest.kt
@@ -3,78 +3,83 @@ package action
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 import java.lang.IllegalStateException
+import javax.print.attribute.standard.NumberOfInterveningJobs
 
 internal class InsertAtStartTest {
-    private val testStorage = CommandStorage()
+    private val testStorage = CommandStorage<Int>()
+    private val testNumberList: MutableList<Int> = mutableListOf()
 
     @Test
     fun doAction() {
-        InsertAtStart(3).doAction(testStorage)
-        InsertAtStart(2).doAction(testStorage)
-        InsertAtStart(1).doAction(testStorage)
-        assertEquals(listOf(1, 2, 3), testStorage.numberList)
+        InsertAtStart(3).doAction(testNumberList, testStorage)
+        InsertAtStart(2).doAction(testNumberList, testStorage)
+        InsertAtStart(1).doAction(testNumberList, testStorage)
+        assertEquals(listOf(1, 2, 3), testNumberList)
     }
 
     @Test
     fun undoAction() {
-        InsertAtStart(3).doAction(testStorage)
-        InsertAtStart(2).doAction(testStorage)
-        InsertAtStart(1).doAction(testStorage)
-        testStorage.undoLastAction()
-        assertEquals(listOf(2, 3), testStorage.numberList)
+        InsertAtStart(3).doAction(testNumberList, testStorage)
+        InsertAtStart(2).doAction(testNumberList, testStorage)
+        InsertAtStart(1).doAction(testNumberList, testStorage)
+        testStorage.undoLastAction(testNumberList)
+        assertEquals(listOf(2, 3), testNumberList)
     }
 }
 
 internal class InsertAtEndTest {
-    private val testStorage = CommandStorage()
+    private val testStorage = CommandStorage<Int>()
+    private val testNumberList: MutableList<Int> = mutableListOf()
+
 
     @Test
     fun doAction() {
-        InsertAtEnd(3).doAction(testStorage)
-        InsertAtEnd(2).doAction(testStorage)
-        InsertAtEnd(1).doAction(testStorage)
-        assertEquals(listOf(3, 2, 1), testStorage.numberList)
+        InsertAtEnd(3).doAction(testNumberList, testStorage)
+        InsertAtEnd(2).doAction(testNumberList, testStorage)
+        InsertAtEnd(1).doAction(testNumberList, testStorage)
+        assertEquals(listOf(3, 2, 1), testNumberList)
     }
 
     @Test
     fun undoAction() {
-        InsertAtEnd(3).doAction(testStorage)
-        InsertAtEnd(2).doAction(testStorage)
-        InsertAtEnd(1).doAction(testStorage)
-        testStorage.undoLastAction()
-        assertEquals(listOf(3, 2), testStorage.numberList)
+        InsertAtEnd(3).doAction(testNumberList, testStorage)
+        InsertAtEnd(2).doAction(testNumberList, testStorage)
+        InsertAtEnd(1).doAction(testNumberList, testStorage)
+        testStorage.undoLastAction(testNumberList)
+        assertEquals(listOf(3, 2), testNumberList)
     }
 }
 
 internal class MoveTest {
-    private val testStorage = CommandStorage()
+    private val testStorage = CommandStorage<Int>()
+    private val testNumberList: MutableList<Int> = mutableListOf()
 
     @Test
     fun doAction() {
-        InsertAtEnd(3).doAction(testStorage)
-        InsertAtEnd(2).doAction(testStorage)
-        InsertAtEnd(1).doAction(testStorage)
-        Move(0, 2).doAction(testStorage)
-        assertEquals(listOf(2, 1, 3), testStorage.numberList)
+        InsertAtEnd(3).doAction(testNumberList, testStorage)
+        InsertAtEnd(2).doAction(testNumberList, testStorage)
+        InsertAtEnd(1).doAction(testNumberList, testStorage)
+        Move<Int>(0, 2).doAction(testNumberList, testStorage)
+        assertEquals(listOf(2, 1, 3), testNumberList)
     }
 
     @Test
     fun undoAction() {
-        InsertAtEnd(3).doAction(testStorage)
-        InsertAtEnd(2).doAction(testStorage)
-        InsertAtEnd(1).doAction(testStorage)
-        Move(0, 2).doAction(testStorage)
-        testStorage.undoLastAction()
-        assertEquals(listOf(3, 2, 1), testStorage.numberList)
+        InsertAtEnd(3).doAction(testNumberList, testStorage)
+        InsertAtEnd(2).doAction(testNumberList, testStorage)
+        InsertAtEnd(1).doAction(testNumberList, testStorage)
+        Move<Int>(0, 2).doAction(testNumberList, testStorage)
+        testStorage.undoLastAction(testNumberList)
+        assertEquals(listOf(3, 2, 1), testNumberList)
     }
 
     @Test
     fun useIncorrectData() {
-        InsertAtEnd(3).doAction(testStorage)
-        InsertAtEnd(2).doAction(testStorage)
-        InsertAtEnd(1).doAction(testStorage)
+        InsertAtEnd(3).doAction(testNumberList, testStorage)
+        InsertAtEnd(2).doAction(testNumberList, testStorage)
+        InsertAtEnd(1).doAction(testNumberList, testStorage)
         assertThrows(IllegalStateException("ERROR! Out of List bounds")::class.java) {
-            Move(-1, 5).doAction(testStorage)
+            Move<Int>(-1, 5).doAction(testNumberList, testStorage)
         }
     }
 }
--- src/test/kotlin/action/CommandStorageTest.kt
+++ src/test/kotlin/action/CommandStorageTest.kt
@@ -7,34 +7,35 @@ import java.io.File
 import java.nio.file.Path
 
 internal class CommandStorageTest {
+    private val testNumberList: MutableList<Int> = mutableListOf()
 
     @Test
     fun doUndoAction() {
-        val testStorage = CommandStorage()
-        InsertAtStart(1).doAction(testStorage)
-        InsertAtEnd(2).doAction(testStorage)
-        Move(0, 1).doAction(testStorage)
-        InsertAtStart(5).doAction(testStorage)
-        testStorage.undoLastAction()
-        assertEquals(listOf(2, 1), testStorage.numberList)
+        val testStorage = CommandStorage<Int>()
+        InsertAtStart(1).doAction(testNumberList, testStorage)
+        InsertAtEnd(2).doAction(testNumberList, testStorage)
+        Move<Int>(0, 1).doAction(testNumberList, testStorage)
+        InsertAtStart(5).doAction(testNumberList, testStorage)
+        testStorage.undoLastAction(testNumberList)
+        assertEquals(listOf(2, 1), testNumberList)
     }
 
     @Test
     fun serializeToFile(@TempDir directory: Path) {
-        val testStorage = CommandStorage()
+        val testStorage = CommandStorage<Int>()
         val file = directory.resolve("testFile.json")
-        InsertAtStart(1).doAction(testStorage)
-        InsertAtStart(2).doAction(testStorage)
-        InsertAtStart(3).doAction(testStorage)
-        InsertAtEnd(5).doAction(testStorage)
+        InsertAtStart(1).doAction(testNumberList, testStorage)
+        InsertAtStart(2).doAction(testNumberList, testStorage)
+        InsertAtStart(3).doAction(testNumberList, testStorage)
+        InsertAtEnd(5).doAction(testNumberList, testStorage)
         testStorage.serializeToFile(file.toString())
         assertEquals(javaClass.getResource("ActionList.json").readText(), File(file.toString()).readText())
     }
 
     @Test
     fun deserializeFromFile() {
-        val testStorage = CommandStorage()
-        testStorage.deserializeFromFile(javaClass.getResource("ActionList.json").path)
-        assertEquals(listOf(3, 2, 1, 5), testStorage.numberList)
+        val testStorage = CommandStorage<Int>()
+        testStorage.deserializeFromFile(testNumberList, javaClass.getResource("ActionList.json").path)
+        assertEquals(listOf(3, 2, 1, 5), testNumberList)
     }
 }
