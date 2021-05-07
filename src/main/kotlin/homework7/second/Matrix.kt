fd464881750598337f7e53f8fbc27dd6a36a7d94
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2021-05-08 02:04:22 +0300

    init

---
+++ src/main/kotlin/homework7/second/Matrix.kt
@@ -0,0 +1,59 @@
+package homework7.second
+
+import java.lang.IllegalArgumentException
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+data class Matrix(private val matrix: Array<IntArray>) {
+
+    private fun isCorrectMatrices(other: Matrix) {
+        if (matrix[0].isEmpty() || other.matrix[0].isEmpty())
+            throw IllegalArgumentException("The matrix has to be not empty")
+
+        if (matrix.size != matrix[0].size &&
+            other.matrix.size != other.matrix[0].size
+        ) throw IllegalArgumentException("The matrix has to be square")
+
+        if (matrix[0].size != other.matrix[0].size)
+            throw IllegalArgumentException("TMatrices must be the same size")
+    }
+
+    operator fun times(other: Matrix): Matrix {
+        isCorrectMatrices(other)
+
+        val resultArray = Array(matrix.size) { IntArray(matrix.size) { 0 } }
+        runBlocking {
+            for (i in matrix.indices) {
+                for (j in matrix.indices) {
+                    for (k in matrix.indices) {
+                        launch {
+                            resultArray[i][j] += matrix[i][k] * other.matrix[k][j]
+                        }
+                    }
+                }
+            }
+        }
+        return Matrix(resultArray)
+    }
+
+    override fun toString(): String {
+        var string = ""
+        matrix.forEach { string += it.joinToString(postfix = "\n") }
+        return string
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Matrix
+
+        if (!matrix.contentDeepEquals(other.matrix)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return matrix.contentDeepHashCode()
+    }
+}
---
+++ src/test/kotlin/homework7/second/MatrixTest.kt
@@ -0,0 +1,101 @@
+package homework7.second
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+internal class MatrixTest {
+
+    companion object {
+
+        @JvmStatic
+        fun getIncorrectData(): List<Arguments> = listOf(
+                Arguments.of(
+                        Matrix(arrayOf(intArrayOf()
+                        )),
+                        Matrix(arrayOf(intArrayOf()
+                        ))
+                ),
+                Arguments.of(
+                        Matrix(arrayOf(intArrayOf(1, 2), intArrayOf(3, 4))),
+                        Matrix(arrayOf(intArrayOf(1, 2, 3), intArrayOf(5, 6, 7)))
+                )
+        )
+
+        @JvmStatic
+        fun getCorrectData(): List<Arguments> = listOf(
+                Arguments.of(
+                        Matrix(arrayOf(intArrayOf(7, 1, -3),
+                                intArrayOf(9, 23, -9),
+                                intArrayOf(3, 6, 5)
+                        )),
+                        Matrix(arrayOf(
+                                intArrayOf(-1, 2, -5),
+                                intArrayOf(3, 4, 1),
+                                intArrayOf(0, 1, 2)
+                        )),
+                        Matrix(arrayOf(
+                                intArrayOf(-1, 2, -5),
+                                intArrayOf(3, 4, 1),
+                                intArrayOf(0, 1, 2)
+                        ))
+                ),
+                Arguments.of(
+                        Matrix(arrayOf(
+                                intArrayOf(1, 5),
+                                intArrayOf(-2, 10),
+                        )),
+                        Matrix(arrayOf(
+                                intArrayOf(1, 2),
+                                intArrayOf(3, 1),
+                        )),
+                        Matrix(arrayOf(
+                                intArrayOf(-1, 3),
+                                intArrayOf(1, 1),
+                        ))
+                ),
+                Arguments.of(
+                        Matrix(arrayOf(intArrayOf(46, 8, 7),
+                                intArrayOf(26, 1, -5),
+                                intArrayOf(-49, -2, 13)
+                        )),
+                        Matrix(arrayOf(intArrayOf(9, 3, 5),
+                                intArrayOf(2, 0, 3),
+                                intArrayOf(0, 1, -6)
+                        )),
+                        Matrix(arrayOf(
+                                intArrayOf(1, -1, -1),
+                                intArrayOf(-1, 4, 7),
+                                intArrayOf(8, 1, -1)
+                        ))),
+                Arguments.of(
+                        Matrix(arrayOf(intArrayOf(344, 601, 4280),
+                                intArrayOf(322, -122, -28),
+                                intArrayOf(-13, 52, 351)
+                        )),
+                        Matrix(arrayOf(intArrayOf(8, 159, 5),
+                                intArrayOf(25, 0, 3),
+                                intArrayOf(0, 13, 0)
+                        )),
+                        Matrix(arrayOf(
+                                intArrayOf(1, -5, -1),
+                                intArrayOf(-1, 4, 27),
+                                intArrayOf(99, 1, -1)
+                        )))
+        )
+    }
+
+    @MethodSource("getIncorrectData")
+    @ParameterizedTest(name = "test{index}, {1}")
+    fun matrixTimesIncorrectDataTest(firstMatrix: Matrix, secondMatrix: Matrix) {
+        assertThrows<IllegalArgumentException> { firstMatrix * secondMatrix }
+    }
+
+    @MethodSource("getCorrectData")
+    @ParameterizedTest(name = "test{index}, {1}")
+    fun matrixTimesCorrectDataTest(expectedMatrix: Matrix, firstMatrix: Matrix, secondMatrix: Matrix) {
+        assertEquals(expectedMatrix, firstMatrix * secondMatrix)
+    }
+}
