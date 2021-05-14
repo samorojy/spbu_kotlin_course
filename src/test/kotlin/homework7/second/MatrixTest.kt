ee736b5fd98dd5be212a67857a11dc6889d50d15
Samorojy <71323710+VseslavKasatsky@users.noreply.github.com>
2021-05-14 18:37:19 +0300

    UpdateTest

--- src/test/kotlin/homework7/second/MatrixTest.kt
+++ src/test/kotlin/homework7/second/MatrixTest.kt
@@ -124,10 +124,10 @@ internal class MatrixTest {
             Arguments.of(
                 Matrix(
                     arrayOf(
-                        intArrayOf(4522,727,157,3653),
-                        intArrayOf(6150,997,137,2284),
-                        intArrayOf(16191,2799,-179,-2824),
-                        intArrayOf(2124,439,7,3630)
+                        intArrayOf(4522, 727, 157, 3653),
+                        intArrayOf(6150, 997, 137, 2284),
+                        intArrayOf(16191, 2799, -179, -2824),
+                        intArrayOf(2124, 439, 7, 3630)
                     )
                 ),
                 Matrix(
@@ -146,18 +146,47 @@ internal class MatrixTest {
                         intArrayOf(1, 1, 1, -1)
                     )
                 )
+            ),
+            Arguments.of(
+                Matrix(
+                    arrayOf(
+                        intArrayOf(4558, 841, 283, 3287, 312),
+                        intArrayOf(6198, 1149, 305, 1796, 1239),
+                        intArrayOf(16251, 2989, 31, -3434, 2805),
+                        intArrayOf(2196, 667, 259, 2898, -1029),
+                        intArrayOf(3273, 2941, 2750, -8124, 3429)
+                    )
+                ),
+                Matrix(
+                    arrayOf(
+                        intArrayOf(30, 15, 0, -23, 6),
+                        intArrayOf(40, 10, -1, 12, 8),
+                        intArrayOf(98, -9, 21, 34, 10),
+                        intArrayOf(12, 14, 15, -86, 12),
+                        intArrayOf(15, 0, 4, -2, 132)
+                    )
+                ),
+                Matrix(
+                    arrayOf(
+                        intArrayOf(157, 25, 0, -6, 15),
+                        intArrayOf(-11, 0, 12, 254, 18),
+                        intArrayOf(32, 15, -5, 4, 21),
+                        intArrayOf(1, 1, 1, -1, 24),
+                        intArrayOf(6, 19, 21, -61, 24)
+                    )
+                )
             )
         )
     }
 
     @MethodSource("getIncorrectData")
-    @ParameterizedTest(name = "test{index}, {1}")
+    @ParameterizedTest(name = "incorrectDataTest{index}, {1}")
     fun matrixTimesIncorrectDataTest(firstMatrix: Matrix, secondMatrix: Matrix) {
         assertThrows<IllegalArgumentException> { firstMatrix * secondMatrix }
     }
 
     @MethodSource("getCorrectData")
-    @ParameterizedTest(name = "test{index}, {1}")
+    @ParameterizedTest(name = "correctDataTest{index}, {1}")
     fun matrixTimesCorrectDataTest(expectedMatrix: Matrix, firstMatrix: Matrix, secondMatrix: Matrix) {
         assertEquals(expectedMatrix, firstMatrix * secondMatrix)
     }
