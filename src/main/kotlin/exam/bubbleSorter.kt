86bc5d16e379d5ccab41f5cd2d1c16d8c3f8e79f
samorojy <kasatsky.vseslav@gmail.com>
2021-05-28 12:57:53 +0300

    Initial

---
+++ src/main/kotlin/exam/bubbleSorter.kt
@@ -0,0 +1,28 @@
+package exam
+
+import java.lang.ArithmeticException
+
+/**
+ * Sorts the Iterable using the resulting comparator
+ * returns a sorted list
+ */
+fun <T> Iterable<T>.bubbleSort(comparator: Comparator<T>): List<T> {
+    val list = this.toMutableList()
+    var swap = true
+    while (swap) {
+        swap = false
+        for (i in 0 until list.lastIndex) {
+            val compareResult = try {
+                comparator.compare(list[i], list[i + 1])
+            } catch (e: ArithmeticException) {
+                println("comparison error")
+                return emptyList()
+            }
+            if (compareResult > 0) {
+                list[i] = list[i + 1].also { list[i + 1] = list[i] }
+                swap = true
+            }
+        }
+    }
+    return list
+}
