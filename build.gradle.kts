c90dcc63252f3ce1aee1845c62be41ef2c2f217f
Vseslav Kasatskiy <71323710+samorojy@users.noreply.github.com>
2021-06-04 22:27:24 +0300

    Update build.gradle.kts
    all Важно

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,6 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 import java.net.URL
 
 plugins {
-    kotlin("jvm") version "1.4.31"
-    kotlin("plugin.serialization") version "1.4.31"
-    id("io.gitlab.arturbosch.detekt") version "1.15.0"
-    id("org.jetbrains.dokka") version "1.4.20"
     kotlin("jvm") version "1.4.32"
     kotlin("plugin.serialization") version "1.4.32"
     id("io.gitlab.arturbosch.detekt") version "1.16.0"
